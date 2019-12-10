0640e8b52466a7534de4598dab4e98591a795768
Paul Leugemors <pleugemors@yahoo.com>
2019-12-10 10:45:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,9 +82,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  programmer_hash[:yukihiro_matsumoto] => {
-    known_for: "Ruby",
-    languages: ["LISP", "C"]
+  programmer_hash[:yukihiro_matsumoto] = {}
+  programmer_hash[:yukihiro_matsumoto][:known_for]= "Ruby"
+  programmer_hash[:yukihiro_matsumoto][:languages]=[]
+  programmer_hash[:yukihiro_matsumoto][:languages] << "LISP"
+  programmer_hash[:yukihiro_matsumoto][:languages] << "C"
   }
     
 end
